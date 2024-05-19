fdf5a85c63183f5b977ef34b1e281e030b2744fe
Sreyash Rajalingam Ananth <rasreyash@gmail.com>
2024-05-19 12:41:14 +0100

    Online Subsystem Steam configured
    Online Subsystem Steam plugin added and configured

--- Source/MultiplayerSessions/MultiplayerSessions.Build.cs
+++ Source/MultiplayerSessions/MultiplayerSessions.Build.cs
@@ -8,7 +8,7 @@ public class MultiplayerSessions : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "OnlineSubsystem", "OnlineSubsystemSteam" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
